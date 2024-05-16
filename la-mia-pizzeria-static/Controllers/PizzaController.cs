9aef50cdaa1329ede25ee9e7349878be85c90499
CalinCretu <calincretu1412@gmail.com>
2024-05-16 14:28:11 +0200

    ADD - PizzaManager.cs creation & functionality

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -7,33 +7,15 @@ namespace la_mia_pizzeria_static.Controllers
 {
     public class PizzaController : Controller
     {
+        private readonly ILogger<PizzaController> _logger;
+
+        public PizzaController(ILogger<PizzaController> logger)
+        {
+            _logger = logger;
+        }
         public IActionResult Index()
         {
-            using PizzasContext db = new PizzasContext();
-
-            if (!db.Pizzas.Any())
-            {
-                List<Pizzas> DbPizza = new List<Pizzas>
-                {
-                    new Pizzas("Quattro Stagioni", "Pomodoro, fiordilatte, funghi, prosciutto cotto, carciofi e olive nere", "~/img/quattro-stagioni.jpg", 7),
-                    new Pizzas("Capricciosa", "Pomodoro, fiordilatte, funghi, prosciutto cotto, carciofi e olive", "~/img/capricciosa.jpg", 7),
-                    new Pizzas("Margherita D.O.P.", "Pomodoro San Marzano D.O.P., mozzarella di bufala campana D.O.P. e basilico fresco", "~/img/margherita-dop.jpg", 8),
-                    new Pizzas("Ortolana", "Pomodoro, fiordilatte, melanzane, zucchine, peperoni e cipolla", "~/img/ortolana.jpg", 6.5),
-                    new Pizzas("Tonno e Cipolla", "Pomodoro, fiordilatte, tonno e cipolla", "~/img/tonno-cipolla.jpg", 6),
-                    new Pizzas("Quattro Formaggi", "Pomodoro, fiordilatte, gorgonzola, provola affumicata, pecorino romano e parmigiano reggiano", "~/img/quattro-formaggi.jpg", 7.5),
-                    new Pizzas("Calzone", "Pomodoro, fiordilatte, prosciutto cotto e funghi, chiusa a calzone", "~/img/calzone.jpg", 7),
-                    new Pizzas("Rustica", "Pomodoro, fiordilatte, salsiccia, patate e rosmarino", "~/img/rustica.jpg", 6.5),
-                    new Pizzas("Tartufata", "Pomodoro, fiordilatte, funghi champignon, prosciutto crudo e crema di tartufo", "~/img/tartufata.jpg", 8),
-                    new Pizzas("Salsiccia e Friarielli", "Pomodoro, fiordilatte, salsiccia e friarielli", "~/img/salsiccia-friarielli.jpg", 7)
-                };
-                foreach (Pizzas pizza in DbPizza)
-                {
-                    db.Add(pizza);
-                }
-                db.SaveChanges();
-            }
-            List<Pizzas> pizzas = db.Pizzas.ToList();
-            return View("Index", pizzas);
+            return View(PizzaManager.GetAllPizzas());
         }
 
         public IActionResult VediPizza(int id)
